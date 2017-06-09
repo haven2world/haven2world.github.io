c82717478a821878526e79aa5eb40d9d1960a0d9
chenhaowen <chenhaowen@kingsoft.com>
2017-06-09 13:36:20 +0800

    1

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { DatePicker } from 'antd';
-
+import { DatePicker,Button } from 'antd';
+if(!localStorage.num)
+localStorage.num = 1;
 function App() {
   return (
     <div style={{ margin: 100 }}>
       <h1>AntDesign Demo</h1>
+      <Button onClick={()=>{localStorage.num = parseInt(localStorage.num) + 1;console.log(localStorage.num);}}>{localStorage.num}</Button>
       <hr /><br />
-      <DatePicker />
+
     </div>
   );
 }
